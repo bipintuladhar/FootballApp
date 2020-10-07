6e749fc7a25575a4e8e96d6b1528682b5ac620bb
Bipin Tuladhar <bipintuladhar314@gmail.com>
2020-10-07 21:48:50 +0545

    Updates

---
+++ precache-manifest.da8f0a8f7fc7ef6c307469e086ed0fb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "455f06ee7e5256ec62deb09512a2110c",
+    "url": "/FootballApp/index.html"
+  },
+  {
+    "revision": "44ff858ba80bc80743c8",
+    "url": "/FootballApp/static/js/2.5623fc71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FootballApp/static/js/2.5623fc71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc2653159ca25022177",
+    "url": "/FootballApp/static/js/main.45f38c39.chunk.js"
+  },
+  {
+    "revision": "53c4dcc9b2d7014b7c0c",
+    "url": "/FootballApp/static/js/runtime-main.7b011cfb.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/FootballApp/precache-manifest.da8f0a8f7fc7ef6c307469e086ed0fb8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FootballApp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
